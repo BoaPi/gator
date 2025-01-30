94f519ba561087567995a9c11d9c60049bf12d80
boapi <tobias.weyer1205@gmail.com>
2025-01-30 14:58:52 +0100

    add periodic fetching of feeds

--- handler_agg.go
+++ handler_agg.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
 
+	"github.com/BoaPi/gator/internal/database"
 	"github.com/BoaPi/gator/internal/rss"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch RSS from %s: %w", cmd.Args[0], err)
+	if len(cmd.Args) != 1 {
+		fmt.Errorf("usage: %s <1s, 1m, 1h>", cmd.Name)
 	}
 
-	fmt.Printf("Feed: %+v\n", feed)
+	time_between_reqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid time %s: %w", cmd.Args[0], err)
+	}
+
+	fmt.Printf("Collecting feeds every %s\n", time_between_reqs)
+
+	ticker := time.NewTicker(time_between_reqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feed to fetch: %w", err)
+	}
+
+	rss, err := rss.FetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return fmt.Errorf("couldn't fetch feed: %w", err)
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		ID: feed.ID,
+		LastFetchedAt: sql.NullTime{
+			Time:  time.Now().UTC(),
+			Valid: true,
+		},
+		UpdatedAt: time.Now().UTC(),
+	})
+	if err != nil {
+		fmt.Errorf("couldn't mark feed as fetched: %w", err)
+	}
+
+	fmt.Printf("Feed %s fetched:", feed.Name)
+	for _, item := range rss.Channel.Item {
+		fmt.Printf(" * %s\n", item.Title)
+	}
+	fmt.Println("================================")
+
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -17,3 +17,12 @@ SELECT * FROM feeds;
 SELECT * FROM feeds
 WHERE $1 = url; 
 
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $2, updated_at = $3
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
